ba810adc103404d0039f456b0d3b97493b5dd181
Martin Zenzes <martin.zenzes@dfki.de>
2014-12-17 14:13:05 +0100

    add comment
    Signed-off-by: Martin Zenzes <martin.zenzes@dfki.de>

--- init.rb
+++ init.rb
@@ -1,2 +1,8 @@
 require 'autoproj/gitorious'
+
+#without this variable beeing set autoproj will complain:
+#
+#   in /home/mzenzes/TransTerrA/rock-trusty-amd64/autoproj/overrides.yml: the source specification {:github=>"rock-core/package_set"} misses either the VCS type or an URL
+#
+# but later autoproj seems to know about github and gitorious and asks for method of access...
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
